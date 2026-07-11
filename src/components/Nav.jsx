043954783053452a6056ac7e92874adf67501f7d
Michael A. Cojocari <michael.cojocari@gmail.com>
2026-07-11 17:16:45 -0400

    Fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,4 @@
+import { withBase } from '../basePath'
 import ThemeMenu from './ThemeMenu.jsx'
 
 export default function Nav({ theme, onThemeChange }) {
@@ -7,16 +8,14 @@ export default function Nav({ theme, onThemeChange }) {
         <a href="#top" className="brand">
           <img src={withBase('/favicon.svg')} alt="" />
           <span>Camillia-MT</span>
-
         </a>
         <div className="nav-spacer" />
         <ThemeMenu selected={theme} onChange={onThemeChange} />
-        <a href="#features">Features</a>
         <a href="#devices">Devices</a>
         <a href="#screenshots">Screenshots</a>
+        <a href="#themes">Themes</a>
         <a href="#flash">Flash</a>
         <a href="https://github.com/oumike/camillia-mt" target="_blank" rel="noreferrer">GitHub</a>
-
       </div>
     </header>
   )
